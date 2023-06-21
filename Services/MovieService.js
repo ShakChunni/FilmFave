15d1967c8b4dd09b792e36b591fd4eebd279ca0c
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-06-22 05:04:09 +0600

    working on api

--- Services/MovieService.js
+++ Services/MovieService.js
@@ -1,4 +1,16 @@
-import {BASE_URL, IMAGE_BASE_URL, API_KEY, ENDPOINTS} from './URLs';
-import axios from 'axios';
+import { BASE_URL, IMAGE_BASE_URL, API_KEY, ENDPOINTS } from "./URLs";
+import axios from "axios";
 
-const axios = require('axios').default;
+const axios = require("axios").default;
+
+const TMDB_HTTP_REQUEST = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    api_key: API_KEY,
+  },
+});
+
+const getNowPlayingMovies = () => TMDB_HTTP_REQUEST.get(ENDPOINTS.NOWPLAYING);
+const getPoster = (path) => `${IMAGE_BASE_URL}/original${path}`;
+
+export default { getNowPlayingMovies, getPoster };
