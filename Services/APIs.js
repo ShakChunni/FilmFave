d048276d5f2f94272203ff65fb8f5a347d328e29
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-06-23 22:17:34 +0600

    working on the movieDetails

--- Services/APIs.js
+++ Services/APIs.js
@@ -1,6 +1,6 @@
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
-
+//dsadjsak
 const API_KEY = "cc76c5c345fe442173a4290e48edd347";
 
 const ENDPOINTS = {
