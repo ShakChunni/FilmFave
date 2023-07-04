ddae7d8738fc6d72a70b8601e61983e00c389c06
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-07-04 17:11:59 +0600

    working on the movieDetails

--- Services/APIs.js
+++ Services/APIs.js
@@ -4,9 +4,9 @@ const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
 const API_KEY = "cc76c5c345fe442173a4290e48edd347";
 
 const ENDPOINTS = {
-    NOW_PLAYING_MOVIES: "/movie/now_playing",
-    UPCOMING_MOVIES: "/movie/upcoming",
-    GENRES: "/genre/movie/list",
+  NOW_PLAYING_MOVIES: "/movie/now_playing",
+  UPCOMING_MOVIES: "/movie/upcoming",
+  GENRES: "/genre/movie/list",
 };
-//aro kaaj korbo
-export { TMDB_BASE_URL, TMDB_IMAGE_BASE_URL, API_KEY, ENDPOINTS}
+
+export { TMDB_BASE_URL, TMDB_IMAGE_BASE_URL, GENRES, API_KEY, ENDPOINTS };
