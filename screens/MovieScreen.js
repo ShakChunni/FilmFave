c0563d899a692f9840955c07972f26edf979fcd5
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-07-17 18:32:45 +0600

    working on the movieDetails

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 
 const MovieScreen = ({ route, navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <StatusBar style="light" translucent={false} />
       <Text>MovieScreen</Text>
     </View>
@@ -12,4 +13,10 @@ const MovieScreen = ({ route, navigation }) => {
 
 export default MovieScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
