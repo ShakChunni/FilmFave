5b10de1f72387c7ca8aeafe56e41dc2b361ff695
ShakChunni <f.m.ashfaq@g.bracu.ac.bd>
2023-07-25 05:33:45 +0600

    adding search bar

--- Services/APIs.js
+++ Services/APIs.js
@@ -2,6 +2,7 @@ const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
 const GENRES = "https://api.themoviedb.org/3/genre/movie/list";
 const youtubeURL = "https://www.youtube.com/watch";
+const SEARCH_MOVIES = "/search/movie";
 
 const API_KEY = "cc76c5c345fe442173a4290e48edd347";
 
@@ -26,4 +27,5 @@ export {
   ENDPOINTS,
   appendToResponse,
   youtubeURL,
+  SEARCH_MOVIES,
 };
